bcdf2e42039ff80f046960c2efc7fda4b0e13a33
Prateek Tyagi <prateek.cultureholidays@gmail.com>
2025-01-14 16:36:27 +0530

    1111111

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT,
     apiUrl: process.env.API_URL ,
     apiurl2 : process.env.API_URL2,
 };
